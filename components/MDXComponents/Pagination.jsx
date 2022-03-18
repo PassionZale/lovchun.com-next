facd0fe3b0d96322b32259f5bd66108bedabd455
Zhang Lei <372470745@qq.com>
2022-03-18 10:39:06 +0800

    chore: 优化底部样式

--- components/MDXComponents/Pagination.jsx
+++ components/MDXComponents/Pagination.jsx
@@ -2,27 +2,33 @@ import Link from 'next/link'
 import { RiArrowLeftCircleFill, RiArrowRightCircleFill } from 'react-icons/ri'
 
 const Pagination = ({ previous, next }) => {
-  if (!previous?.title && !next?.title) return null
+  if (!previous && !next) return null
 
   return (
-    <div className="mt-10 flex items-center justify-between text-sm font-semibold text-slate-700 dark:text-slate-200">
-      {previous?.title && (
+    <div className="mt-10 flex items-center justify-between space-x-3 text-sm font-semibold text-slate-700 dark:text-slate-200">
+      <div
+        className={`flex w-full justify-start ${
+          previous ? 'visible' : 'invisible'
+        }`}
+      >
         <Link href={`/posts/${previous?.slug}`} passHref>
-          <button className="flex items-center border px-4 py-2 hover:text-slate-900 dark:hover:text-white">
+          <button className="group flex items-center hover:text-slate-900 dark:hover:text-white">
             <RiArrowLeftCircleFill className="mr-3 overflow-visible text-slate-400 group-hover:text-slate-600 dark:group-hover:text-slate-300" />
             {previous?.title}
           </button>
         </Link>
-      )}
+      </div>
 
-      {next?.title && (
+      <div
+        className={`flex w-full justify-end ${next ? 'visible' : 'invisible'}`}
+      >
         <Link href={`/posts/${next?.slug}`} passHref>
-          <button className="flex items-center border px-4 py-2 hover:text-slate-900 dark:hover:text-white">
+          <button className="group flex items-center hover:text-slate-900 dark:hover:text-white">
             {next?.title}
             <RiArrowRightCircleFill className="ml-3 overflow-visible text-slate-400 group-hover:text-slate-600 dark:group-hover:text-slate-300" />
           </button>
         </Link>
-      )}
+      </div>
     </div>
   )
 }
