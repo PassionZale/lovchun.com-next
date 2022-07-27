af73c68a41c535a38a75279beedf079145e22857
Zhang Lei <372470745@qq.com>
2022-07-27 09:34:48 +0800

    add: npprogress

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,40 @@
 import '@/styles/globals.css'
+import '@/styles/nprogress.css'
 import '@/styles/prism.css'
 import 'prism-themes/themes/prism-atom-dark.css'
 
+import { useEffect } from 'react'
 import Head from 'next/head'
 import { ThemeProvider } from 'next-themes'
+import { useRouter } from 'next/router'
+import NProgress from 'nprogress'
 import { IconContext } from 'react-icons'
 
 import BaseLayout from '@/components/BaseLayout'
 
 const App = ({ Component, pageProps }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleStart = (url) => {
+      console.log(`Loading: ${url}`)
+      NProgress.start()
+    }
+    const handleStop = () => {
+      NProgress.done()
+    }
+
+    router.events.on('routeChangeStart', handleStart)
+    router.events.on('routeChangeComplete', handleStop)
+    router.events.on('routeChangeError', handleStop)
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart)
+      router.events.off('routeChangeComplete', handleStop)
+      router.events.off('routeChangeError', handleStop)
+    }
+  }, [router])
+
   return (
     <>
       <Head>
