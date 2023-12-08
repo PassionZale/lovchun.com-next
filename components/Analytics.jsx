510b1c5ff68f1546ae156ef92106e64b6001f2f8
PassionZale <372470745@qq.com>
2023-12-08 08:37:49 +0800

    refactor: @vercel/speed-insights

--- components/Analytics.jsx
+++ components/Analytics.jsx
@@ -1,11 +1,5 @@
 import { Analytics } from '@vercel/analytics/react'
-import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export function AnalyticsWrapper() {
-  return (
-    <>
-      <Analytics />
-      <SpeedInsights />
-    </>
-  )
+  return <Analytics />
 }
