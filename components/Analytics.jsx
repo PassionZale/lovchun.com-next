0b600dec5e92c41b56cbaad9f1f6e9e3f31cec28
PassionZale <372470745@qq.com>
2023-12-08 08:30:06 +0800

    add: @vercel/speed-insights/next

--- components/Analytics.jsx
+++ components/Analytics.jsx
@@ -1,5 +1,11 @@
 import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 
 export function AnalyticsWrapper() {
-  return <Analytics />
+  return (
+    <>
+      <Analytics />
+      <SpeedInsights />
+    </>
+  )
 }
