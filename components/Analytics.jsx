014d765b69ad3e5772660ba94ae83618525cd52c
PassionZale <372470745@qq.com>
2024-01-04 09:48:21 +0800

    empty repo

--- components/Analytics.jsx
+++
@@ -1,5 +0,0 @@
-import { Analytics } from '@vercel/analytics/react'
-
-export function AnalyticsWrapper() {
-  return <Analytics />
-}
--- components/MDXComponents/DynamicComponents/ThemeSwitch.jsx
+++
@@ -1,4 +0,0 @@
-// 复制一份 ThemeSwitch 以供 /about 使用
-import ThemeSwitch from '@/components/ThemeSwitch'
-
-export default ThemeSwitch
