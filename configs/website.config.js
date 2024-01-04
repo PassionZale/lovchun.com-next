fd335be618d8b1aaf342c8679d049df2c4db57fc
PassionZale <372470745@qq.com>
2024-01-04 08:59:46 +0800

    feat: 2023-end-of-year

--- configs/website.config.js
+++ configs/website.config.js
@@ -2,7 +2,7 @@ const WEBSITE = {
   author: 'Lei Zhang',
   avatar: '/static/images/avatar.webp',
   title: 'Lei Zhang - lovchun.com',
-  description: "2024，33/35。",
+  description: "2024，33/35，稳工！",
   lang: 'zh-cn',
 
   // icp: '粤ICP备16020962号-1', 我的备案没有了 T_T
