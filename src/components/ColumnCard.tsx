3065d1574e54265dadf4c8fde8da78e5766c39fe
Zhang Lei <372470745@qq.com>
2024-03-21 09:50:47 +0800

    feat: 专栏

--- src/components/ColumnCard.tsx
+++ src/components/ColumnCard.tsx
@@ -1,23 +1,18 @@
-interface Props {
-  title: string;
-  description: string;
-  createDatetime: string;
-  count: number;
-  href: string;
-}
+import type { Column } from "@utils/getUniqueColumns";
+
+interface Props extends Column {}
 
 export default function GithubRepoCard({
   title,
   description,
   createDatetime,
   count,
-  href,
+  slug,
 }: Props) {
   return (
     <div className="flex h-full flex-col rounded border-2 border-skin-fill/50 p-4 transition-all hover:scale-[1.01] hover:bg-skin-card-muted/40">
       <a
-        href={href}
-        target="_blank"
+        href={`/columns/${slug}`}
         rel="noreferrer"
         className="cursor-pointer font-medium"
       >
@@ -25,7 +20,7 @@ export default function GithubRepoCard({
           <span className="text-skin-accent">{title}</span>
         </div>
         <div className="mb-4 mt-2">{description}</div>
-        <div className="mt-auto">
+        <div className="mt-auto opacity-80 text-sm">
           创建于&nbsp;{createDatetime}，共&nbsp;{count}&nbsp;篇文章
         </div>
       </a>
