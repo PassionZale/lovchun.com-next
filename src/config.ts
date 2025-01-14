dcb5bd5c4ec9d075f7672cfd036c1cebab637dfc
Zhang Lei <372470745@qq.com>
2025-01-14 15:06:43 +0800

    config: talks

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,16 @@
-import type { Site, SocialObjects } from "./types";
+import type { Site, SocialObjects, Talk } from "./types";
 
 export const SITE: Site = {
   website: "https://www.lovchun.com",
   author: "Lei Zhang",
-	profile: "https://www.lovchun.com",
+  profile: "https://www.lovchun.com",
   desc: "Articles & happiness I want to share.",
   title: "Lei Zhang",
   lightAndDarkMode: true,
   postPerIndex: 4,
   postPerPage: 8,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
-	showArchives: true,
+  showArchives: true,
   editPost: {
     url: "https://github.com/PassionZale/lovchun.com-next/edit/main/src/data/blog",
     text: "修改",
@@ -66,4 +66,21 @@ export const REPOS: `${string}/${string}`[] = [
   "PassionZale/type-challenges",
   "PassionZale/iMall",
   "PassionZale/JWT-RESTfull-IN-CI-Tutorial",
+];
+
+export const TALKS: Talk[] = [
+  {
+    date: "2023-12-08",
+    title: "Setup Miniprogram",
+    place: "武汉",
+    preview: "https://talks.lovchun.com/2023/setup-miniprogram",
+    download: "https://talks.lovchun.com/2023/setup-miniprogram/pdf",
+  },
+  {
+    date: "2024-03-10",
+    title: "Happy Coding For 10 Years",
+    place: "武汉",
+    preview: "https://talks.lovchun.com/2024/happy-coding-for-10-years",
+    download: "https://talks.lovchun.com/2024/happy-coding-for-10-years/pdf",
+  },
 ];
