2c284ddb4b7114c70fc0c5d8abf84562b12c8370
Zhang Lei <372470745@qq.com>
2025-01-16 10:41:33 +0800

    chore: 调整 astro.config.ts 文件中的导入顺序

--- astro.config.ts
+++ astro.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
-import { SITE } from "./src/config";
 import rehypePrettyCode from "rehype-pretty-code";
-import moonlight from "./public/assets/moonlight-ii.json";
 import { transformerCopyButton } from "@rehype-pretty/transformers";
+import { SITE } from "./src/config";
+import moonlight from "./public/assets/moonlight-ii.json";
 
 export default defineConfig({
   site: SITE.website,
